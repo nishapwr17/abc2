830583f5a3d7aa7e60965cc7e05849c455e25056
nishapwr17 <nishapwr17@gmail.com>
2024-09-27 17:01:32 +0530

    Update Test.java

--- Hello/src/com/test/Test.java
+++ Hello/src/com/test/Test.java
@@ -8,7 +8,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello nisha");
-		System.out.println("hello");
+		
+		
 		
 	}
 
