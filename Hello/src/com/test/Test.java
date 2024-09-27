8f4f87131dfe37a6f8405c1c2e4a999e75be77b3
honmuteneha <honmuteneha@gmail.com>
2024-09-27 16:08:39 +0530

    Update Test.java

--- Hello/src/com/test/Test.java
+++ Hello/src/com/test/Test.java
@@ -1,6 +1,10 @@
 package com.test;
 
 public class Test {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello nisha");
